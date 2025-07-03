2c0c9a8530c5b5369d1e1e296e96346b2b507ca1
Skway <livesg815@gmail.com>
2025-07-03 15:06:40 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA0bD721mrYBoioCA9NKxJPWcJ0VdNqar4",
+    authDomain: "missao-impossivel-62aaa.firebaseapp.com",
+    projectId: "missao-impossivel-62aaa",
+    storageBucket: "missao-impossivel-62aaa.firebasestorage.app",
+    messagingSenderId: "268896227387",
+    appId: "1:268896227387:web:b58dab9e51f7217e35cc1e",
+    measurementId: "G-NXQ6DCMF12"
+};
+
+initializeApp(firebaseConfig);
